11e13b2a7b29fd2c72facce8b0c7edf7b81c6803
Danil <frolovdanil75@gmail.com>
2023-12-03 17:07:35 +0400

    added view model for save functionality

--- NumberFacts/app/src/main/java/com/example/numberfacts/MainActivity.kt
+++ NumberFacts/app/src/main/java/com/example/numberfacts/MainActivity.kt
@@ -5,18 +5,33 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toolbar
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
 import com.example.numberfacts.controllers.MainController
 import com.example.numberfacts.models.NumberFactModel
+import com.example.numberfacts.viewModels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val model = NumberFactModel(this)
     private val controller = MainController(model)
+    private val viewModel: MainViewModel by viewModels()
+
+    private var getFactButton: Button? = null
+    private var editText: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getFactButton = findViewById<Button>(R.id.btnGetFact)
+        editText = findViewById<EditText>(R.id.etNumber)
+        editText?.setText(viewModel.getInputText())
         setContentView(R.layout.activity_main)
         setListeners()
+    }
+
+    override fun onDestroy() {
+        viewModel.saveText(editText?.text.toString())
+        super.onDestroy()
     }
 
     fun openInfoScreen() {
@@ -24,11 +39,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-        val getFactButton = findViewById<Button>(R.id.btnGetFact)
-        val editText = findViewById<EditText>(R.id.etNumber)
-
-        getFactButton.setOnClickListener {
-            controller.getFactButtonPressed(editText.text.toString().toInt())
+        getFactButton?.setOnClickListener {
+            controller.getFactButtonPressed(editText?.text.toString().toInt())
         }
     }
 }
